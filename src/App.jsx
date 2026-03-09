d2167bf199c868233f506e3fcce4a9600211032c
angelostori <angelostori13@gmail.com>
2026-03-09 16:12:45 +0100

    implement the routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layout/DefaultLayout"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import ProjectsDetails from "./pages/ProjectsDetails"
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />} />
+        <Route index element={<Home />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="projects/:id" element={<ProjectsDetails />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
