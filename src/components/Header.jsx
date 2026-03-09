2825d07dc88b8bd81932877391d5c23aab75c186
angelostori <angelostori13@gmail.com>
2026-03-09 16:48:29 +0100

    update header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,24 @@
+import { NavLink } from "react-router-dom"
+
 export default function Header() {
 
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar w/ text</a>
+                    <NavLink className="navbar-brand" to="/">Portfolio</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarText">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
+                                <NavLink className="nav-link" to="/projects">Projects</NavLink>
                             </li>
                         </ul>
-                        <span className="navbar-text">
-                            Navbar text with an inline element
-                        </span>
                     </div>
                 </div>
             </nav>
